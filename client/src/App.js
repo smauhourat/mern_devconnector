328a80dd461324369ae4cebe3361801e5d0679c8
SMauhourat <santiagomauhourat@hotmail.com>
2021-09-24 09:55:49 -0300

    get user

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -6,12 +6,23 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
-const App = () => (
+if (localStorage.token){
+  setAuthToken(localStorage.token);
+} 
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -27,6 +38,6 @@ const App = () => (
       </Fragment>
     </Router>
   </Provider>
-);
+)};
 
 export default App;
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR
 } from '../actions/types';
 
 const initialState = {
@@ -14,15 +16,23 @@ export default function(state = initialState, action) {
     const { type, payload } = action;
 
     switch (type) {
+        case USER_LOADED:
+            return {
+                ...state, action,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token)
             return {
                 ...state, 
-                payload,
+                ...payload,
                 isAuthenticated: true,
                 loading: false
             }
         case REGISTER_FAIL:
+        case AUTH_ERROR:
             localStorage.removeItem('token');
             return {
                 ...state, 
