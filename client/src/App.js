4f4708107f6a1c940db3b56e2aec124d5bdcb091
SMauhourat <santiagomauhourat@hotmail.com>
2021-10-30 19:04:04 -0300

    add experience profile

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperiece from './components/profile-forms/AddExperiece';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -42,6 +43,7 @@ const App = () => {
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute exact path="/create-profile" component={CreateProfile} />
             <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+            <PrivateRoute exact path="/add-experience" component={AddExperiece} />
           </Switch>
         </section>
       </Fragment>
