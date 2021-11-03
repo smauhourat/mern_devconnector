f4e1454934ea4190ebd80f9debc82f6464fa83b9
Santiago Mauhourat <smauhourat@aadim.org.ar>
2021-11-03 17:39:05 -0300

    delete posts when delete account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const { check, validationResult } = require('express-validator');
 const { route } = require('./users');
 
@@ -146,7 +147,7 @@ router.get('/user/:user_id', async (req,res) => {
 router.delete('/', auth, async (req,res) => {
     try {
         // @todo - remove users posts
-
+        await Post.deleteMany({ user: req.user.id });
         // Remove profiles
         await Profile.findOneAndRemove({ user: req.user.id })
         // Remove user
