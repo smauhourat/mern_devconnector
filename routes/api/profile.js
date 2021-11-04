ee561245315f6005b7560e19d7d37118a5dea5be
SMauhourat <santiagomauhourat@hotmail.com>
2021-11-04 16:44:01 -0300

    delete post in delete account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const { check, validationResult } = require('express-validator');
 const { route } = require('./users');
 
@@ -145,8 +146,8 @@ router.get('/user/:user_id', async (req,res) => {
 // @access  Private
 router.delete('/', auth, async (req,res) => {
     try {
-        // @todo - remove users posts
-
+        // Remove users posts
+        await  Post.deleteMany({ user: req.user.id })
         // Remove profiles
         await Profile.findOneAndRemove({ user: req.user.id })
         // Remove user
